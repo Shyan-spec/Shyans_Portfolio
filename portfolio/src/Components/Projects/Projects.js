6d2f4da343fd859c5968f718c1dcc5f034220da9
Shyan Walker <shyanw321@gmail.com>
2022-03-19 22:50:55 -0400

    finished

--- portfolio/src/Components/Projects/Projects.js
+++ portfolio/src/Components/Projects/Projects.js
@@ -1,20 +1,11 @@
 import React ,{Component} from 'react';
 import './Projects.scss';
-import Tilty from 'react-tilty'
-import Particles from 'react-tsparticles';
+import CalcPic from './CalcPic.png';
 
 class Projects extends Component {
     render() {
 
-        const particlesInit = (main) => {
-            console.log(main);
         
-            // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-          };
-        
-          const particlesLoaded = (container) => {
-            console.log(container);
-          };
     return (
                 
                 
@@ -30,8 +21,14 @@ class Projects extends Component {
 
                         <div className="textbox mar">
                             
-                            <h1 className="title">Project 1 Coming Soon</h1>
-                            <span className="description">Project Under Construction...</span>
+                            <h1 className="title">Project 1: <br/> Your Online Calculator</h1>
+                            <div className="desAndbuttonWrap">
+                            <span className="description">A basic online calculator made from scratch! Here to handle all of your mathematic problems.</span>
+
+                            <a href="https://shyan-spec.github.io/Calculator/" target="_blank">
+                            <button className="live">See Live!</button>
+                            </a>
+                            </div>
 
                         </div>
 
@@ -39,31 +36,8 @@ class Projects extends Component {
                     
 
                     
-                    <div className="box mar">
-                    <div className="all-gears">
-                        <div className="gear parent1">
-                                <div class="center"></div>
-                                <div class="tooth"></div>
-                                <div class="tooth"></div>
-                                <div class="tooth"></div>
-                                <div class="tooth"></div>
-                                
-                            </div>
-                            <div className="gear parent2">
-                                <div class="center"></div>
-                                <div class="tooth"></div>
-                                <div class="tooth"></div>
-                                <div class="tooth"></div>
-                                <div class="tooth"></div>
-                            </div>
-                            <div className="gear2 parent3">
-                                <div class="center2"></div>
-                                <div class="tooth2"></div>
-                                <div class="tooth2"></div>
-                                <div class="tooth2"></div>
-                                <div class="tooth2"></div>
-                            </div>
-                        </div>
+                    <div className="box1 mar">
+                    <img alt="Calculator Image" className="Calc-img" src={CalcPic}/>
                         </div>
                         
 
@@ -72,8 +46,11 @@ class Projects extends Component {
 
                         <div className="textbox mar">
                             
-                            <h1 className="title">Project 2 Coming Soon</h1>
-                            <span className="description">Project Under Construction...</span>
+                            <h1 className="title">Project 2</h1>
+                            <div className="desAndbuttonWrap">
+                            <span className="description">Project Under Construction.</span>
+                            <button className="live">Coming Soon...</button>
+                            </div>
 
                         </div>
 
@@ -110,8 +87,11 @@ class Projects extends Component {
                         
                         <div className="textbox mar">
                             
-                            <h1 className="title">Project 3 Coming Soon</h1>
-                            <span className="description">Project Under Construction...</span>
+                            <h1 className="title">Project 3</h1>
+                            <div className="desAndbuttonWrap">
+                            <span className="description">Project Under Construction.</span>
+                            <button className="live">Coming Soon...</button>
+                            </div>
 
                         </div>
 
